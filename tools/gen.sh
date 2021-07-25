1a16e60c828d4fb326bab3ccd3a8d0de379b963d
ivan <ivan@ivk1800.ru>
2021-07-25 23:12:56 +0400

    improve tools

--- tools/gen.sh
+++ tools/gen.sh
@@ -1,22 +1,21 @@
-cd ../app/
-echo 'generate for app'
-flutter packages pub run build_runner build
-cd ../feature/feature_chats_list_impl/
-echo 'generate for feature_chats_list_impl'
-flutter packages pub run build_runner build
-cd ../feature_main_screen_impl/
-echo 'generate for feature_main_screen_impl'
-flutter packages pub run build_runner build
-cd ../feature_chat_impl/
-echo 'generate for feature_chat_impl'
-flutter packages pub run build_runner build
-cd ../feature_settings_impl/
-echo 'generate for feature_settings_impl'
-flutter packages pub run build_runner build
-cd ../feature_dev/
-echo 'generate for feature_dev'
-flutter packages pub run build_runner build
-cd ../feature_chat_settings_impl/
-echo 'generate for feature_chat_settings_impl'
-cd ../feature_wallpappers_impl/
-echo 'generate for feature_wallpappers_impl'
+#!/usr/bin/env bash
+
+function generate() {
+  for d in */; do
+    cd $d
+    pubspec="pubspec.yaml"
+    if [ -f "$pubspec" ]; then
+      if grep -q build_runner "$pubspec"; then
+        echo "$(tput setaf 2)build for $d$(tput sgr0)"
+        flutter packages pub run build_runner build
+      fi
+    fi
+    cd ..
+  done
+}
+
+cd ..
+generate
+
+cd feature/
+generate
--- tools/packages_get.sh
+++ tools/packages_get.sh
@@ -1,71 +1,17 @@
+#!/usr/bin/env bash
 
-cd ../app/
-flutter packages get
-cd ../core/
-flutter packages get
-cd ../core_utils/
-flutter packages get
-cd ../core_arch/
-flutter packages get
-cd ../launch/
-flutter packages get
-cd ../td_client/
-cd ../localization_api/
-flutter packages get
-cd ../localization_impl
-flutter packages get
-cd ../split_view
-flutter packages get
-cd ../coreui/
-flutter packages get
-cd ../fake/
-flutter packages get
-cd ../demo/
-flutter packages get
-cd ../feature/feature_chats_list_api/
-flutter packages get
-cd ../feature_chats_list_impl/
-flutter packages get
-cd ../feature_global_search_api/
-flutter packages get
-cd ../feature_global_search_impl/
-flutter packages get
-cd ../feature_main_screen_api/
-flutter packages get
-cd ../feature_main_screen_impl/
-flutter packages get
-cd ../feature_chat_impl/
-flutter packages get
-cd ../feature_settings_impl/
-flutter packages get
-cd ../feature_settings_api/
-flutter packages get
-cd ../feature_settings_search_impl/
-flutter packages get
-cd ../feature_settings_search_api/
-flutter packages get
-cd ../feature_dev/
-flutter packages get
-cd ../feature_privacy_settings_api/
-flutter packages get
-cd ../feature_privacy_settings_impl/
-flutter packages get
-cd ../feature_notifications_settings_api/
-flutter packages get
-cd ../feature_notifications_settings_impl/
-flutter packages get
-cd ../feature_data_settings_api/
-flutter packages get
-cd ../feature_data_settings_impl/
-flutter packages get
-cd ../feature_chat_settings_api/
-flutter packages get
-cd ../feature_chat_settings_impl/
-flutter packages get
-cd ../feature_wallpappers_api/
-flutter packages get
-cd ../feature_wallpappers_impl/
-flutter packages get
-cd ../feature_stickers_api/
-flutter packages get
-cd ../feature_stickers_impl/
+function sync() {
+  for d in */; do
+    cd $d
+    pubspec="pubspec.yaml"
+    if [ -f "$pubspec" ]; then
+      flutter packages get
+    fi
+    cd ..
+  done
+}
+
+cd ..
+sync
+cd feature/
+sync
