3350c77c0d2b576eb50f5deae74148e793e2c2e9
ivan <ivan@ivk1800.ru>
2021-08-08 22:21:03 +0400

    add block interaction widget

--- coreui/lib/coreui.dart
+++ coreui/lib/coreui.dart
@@ -5,4 +5,5 @@ export 'src/connection_state_widget_factory.dart';
 export 'src/image_widget_factory.dart';
 export 'src/list/tile_factory.dart';
 export 'src/tg_switched_app_bar.dart';
+export 'src/widget/tg/block_interaction.dart';
 export 'src/widget/tg/tg_widget.dart';
---
+++ coreui/lib/src/widget/tg/block_interaction.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class BlockInteraction extends StatelessWidget {
+  const BlockInteraction({
+    Key? key,
+    required this.block,
+    required this.child,
+    this.canPop = false,
+  }) : super(key: key);
+
+  final bool block;
+  final bool canPop;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return canPop;
+      },
+      child: Stack(
+        children: <Widget>[
+          child,
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 200),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return FadeTransition(
+                child: child,
+                opacity: animation,
+              );
+            },
+            child: block
+                ? Container(
+                    color: Colors.black45,
+                    child: Center(
+                      child: Container(
+                          padding: const EdgeInsets.all(20.0),
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(16.0))),
+                          child: const CircularProgressIndicator()),
+                    ),
+                  )
+                : const SizedBox(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+extension BlockInteractionExt on Widget {
+  Widget withBlockInteraction(bool block) {
+    return BlockInteraction(
+      block: block,
+      child: this,
+    );
+  }
+}
