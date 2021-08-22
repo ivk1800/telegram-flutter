aa2fa29d64615e9ccd44d6fb5865dbfe30a04810
ivan <ivan@ivk1800.ru>
2021-08-22 22:42:52 +0400

    update generate dependencies

--- tools/gen.sh
+++ tools/gen.sh
@@ -7,7 +7,7 @@ function generate() {
     if [ -f "$pubspec" ]; then
       if grep -q build_runner "$pubspec"; then
         echo "$(tput setaf 2)build for $d$(tput sgr0)"
-        flutter packages pub run build_runner build
+        flutter packages pub run build_runner build --delete-conflicting-outputs
       fi
     fi
     cd ..
