149ff2e8aa15ffac536e8b69163c40858f2d85e9
ivan <ivan@ivk1800.ru>
2022-01-02 22:16:43 +0400

    fix load backgrounds

--- feature/feature_wallpapers_impl/lib/src/util/background_fill_ext.dart
+++ feature/feature_wallpapers_impl/lib/src/util/background_fill_ext.dart
@@ -4,6 +4,7 @@ import 'background_fill.dart';
 import 'color_rgb24_ext.dart';
 
 extension BackgroundFillExt on td.BackgroundFill {
+  // todo refactor
   IBackgroundFill toBackgroundFill() {
     final td.BackgroundFill fill = this;
     if (fill is td.BackgroundFillSolid) {
@@ -15,6 +16,11 @@ extension BackgroundFillExt on td.BackgroundFill {
         topColor: fill.topColor.toColor(),
         bottomColor: fill.bottomColor.toColor(),
         angle: fill.rotationAngle,
+      );
+    } else if (fill is td.BackgroundFillFreeformGradient) {
+      // todo handle correct color
+      return BackgroundFillSolid(
+        color: fill.colors[0].toColor(),
       );
     }
 
