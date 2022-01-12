06bf35d2fc4479992e0edc516f6f0288f8531bf4
ivan <ivan@ivk1800.ru>
2022-01-12 23:52:03 +0400

    delete deprecated classes

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,8 +1,6 @@
 library core;
 
-export 'package:core/src/mananger/manager.dart';
-export 'package:core/src/view_model/data_loading_handler_mixin.dart';
-export 'package:core/src/view_model/view_model.dart';
+export 'package:core/src/manager/manager.dart';
 
 export 'src/provider/provider.dart';
 export 'src/util/util.dart';
--- core/lib/src/view_model/view_model.dart
+++
@@ -1,3 +0,0 @@
-export 'data_loading_widget.dart';
-export 'data_loading_widget_delegate.dart';
-export 'default_data_loading_widget_delegate_mixin.dart';
