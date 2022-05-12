bdbf7e31521a787a0633e135830ee9081dbbdf8a
ivan <ivan@ivk1800.ru>
2022-05-12 22:05:01 +0400

    temporary disable users cache

--- core_tdlib_impl/lib/src/repository/user_repository_impl.dart
+++ core_tdlib_impl/lib/src/repository/user_repository_impl.dart
@@ -22,10 +22,13 @@ class UserRepositoryImpl extends IUserRepository {
 
   @override
   Future<td.User> getUser(int id) {
+    return _functionExecutor.send<td.User>(td.GetUser(userId: id));
+    /*
     return _cache.putIfAbsent(
       id,
       () => _functionExecutor.send<td.User>(td.GetUser(userId: id)),
     );
+     */
   }
 
   @override
