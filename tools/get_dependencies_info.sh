8d3422bc774c928b527c2861b6126b54e31499c6
ivan <ivan@ivk1800.ru>
2022-11-27 22:23:43 +0400

    update jugger

--- tools/get_dependencies_info.sh
+++ tools/get_dependencies_info.sh
@@ -5,5 +5,5 @@ parent_path=$(
   cd "$(dirname "${BASH_SOURCE[0]}")"
   pwd -P
 )
-echo "get_depencencies_info..."
+echo "get_dependencies_info..."
 dart run "$parent_path/tools-project/lib/main.dart" dependencies_info --work-directory "$parent_path/../"
